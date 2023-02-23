fcded813afa6620f3176128b6a6aac419f34eb55
LuDon89 <daudonlucas@gmail.com>
2023-02-23 14:43:37 +0100

    affichage partenaires

--- views/accueil.php
+++ views/accueil.php
@@ -5,6 +5,15 @@ require('../models/connect-bdd.php');
 
 var_dump($bdd);
 
+// Requête & execution
+$sql = "SELECT * FROM acteurs";
+$req = $bdd->query($sql);
+
+// Lire le résultat de la requête
+$acteurs = $req->fetchAll(PDO::FETCH_ASSOC);
+
+// var_dump($acteurs);
+
 ?>
 
 <!DOCTYPE html>
@@ -21,55 +30,14 @@ var_dump($bdd);
     <body id="page-top">
         <!-- Navigation-->
         <?php require('../template/header.php'); ?>
-        <!-- Content section 1-->
         <section id="scroll">
             <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6 order-lg-2">
-                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/01.jpg" alt="..." /></div>
-                    </div>
-                    <div class="col-lg-6 order-lg-1">
-                        <div class="p-5">
-                            <h2 class="display-4">For those about to rock...</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-                            <button type="submit" class="btn btn-success">En Savoir +</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Content section 2-->
-        <section>
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6">
-                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/02.jpg" alt="..." /></div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="p-5">
-                            <h2 class="display-4">We salute you!</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-                            <button type="submit" class="btn btn-success">En Savoir +</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- Content section 3-->
-        <section>
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6 order-lg-2">
-                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/03.jpg" alt="..." /></div>
-                    </div>
-                    <div class="col-lg-6 order-lg-1">
-                        <div class="p-5">
-                            <h2 class="display-4">Let there be rock!</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-                            <button type="submit" class="btn btn-success">En Savoir +</button>
-                        </div>
-                    </div>
-                </div>
+                <!-- Acteurs/Partenaires -->
+                <?php
+                foreach ($acteurs as $acteur) {
+                    require('../components/accueil_partenaire.php');
+                }
+                ?>
             </div>
         </section>
         <!-- Footer-->
