50b59b224e5b7d7344ecdeb10adc5b6ece1e5df4
ludon89 <daudonlucas@gmail.com>
2023-02-26 20:38:31 +0100

    test merge

--- index.php
+++ index.php
@@ -17,6 +17,8 @@
     <link href="assets/css/styles.css" rel="stylesheet" />
 </head>
 
+<!-- Test merge -->
+
 <body id="page-top">
     <!-- header-->
     <nav class="navbar navbar-expand-lg bg-dark">
