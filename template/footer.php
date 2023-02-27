134ad31174d1d9818d728e470fecf0df22273f24
LuDon89 <daudonlucas@gmail.com>
2023-02-27 16:24:54 +0100

    correction footer

--- template/footer.php
+++ template/footer.php
@@ -1,28 +1,69 @@
-<footer class="py-5 d-flex flex-row">
+<div class="container-expand my-9">
+    <!-- Footer -->
+    <footer class="foot" class="text-center text-lg-start text-white" style="background-color: #929fba">
+        <!-- Grid container -->
+        <div class="container p-9 pb-0">
+            <!-- Section: Links -->
+            <section class="">
+                <!--Grid row-->
+                <div class="row">
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-0 mt-4" style="color: white;">
+                        <h6 class="text-uppercase mb-4 font-weight-bold" style=" font-size : 1.3rem;">Liens utiles</h6>
+                        <p><a href="index.php" style="color : white; text-decoration : none;">Accueil</a></p>
+                        <p><a href="about.php" style="color : white; text-decoration : none;  ">Qui sommes-nous ?</a></p>
+                        <p><a href="" style="color : white; text-decoration : none;  ">Mentions légales</a></p>
+                        <p><a href="" style="color : white; text-decoration : none; ">Politique de confidentialités</a></p>
+                    </div>
 
-    <div class="container popo py-5">
-        <h3 class="display">Liens utiles</h3>
-        <a href="index.php" class="display">Accueil</a>
-        <a href="about.php" class="display">Qui sommes-nous ?</a>
-        <a href="" class="display">Mentions légales</a>
-        <a href="" class="display">Politique de confidentialités</a>
-        <a href="" class="display">C.G.U</a>
-    </div>
-    <div class="iconreseau">
-    <div class="container popo py-5">
-        <h3 class="display">Contact</h3>
-        <p class="display">06.09.08.67.54</p>
-        <p class="display">04.73.90.90.09</p>
-        <p class="display">Gmae@gmail.com</p>
-        <p class="display">10 Allée Evarsite Galois <br> 63000</p>
-    </div>
-    <div class="container pipi py-5">
-        <h3 class="displaypi">G.M.A.E</h3>
-        <div>
-            <i id="in" class="fa-brands fa-linkedin"></i>
-            <i id="insta" class="fa-brands fa-instagram"></i>
-            <i id="face" class="fa-brands fa-facebook"></i>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+
+
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none" />
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-4" style="color: white;">
+                        <h6 class="text-uppercase mb-4 font-weight-bold" style=" font-size : 1.3rem;">Contact</h6>
+                        <p><i class="fas fa-phone mr-3" style=" margin-right : 0.5rem;"></i> 04.73.90.90.09</p>
+                        <p><i class="fas fa-phone mr-3" style=" margin-right : 0.5rem;"></i>06.09.08.67.54</p>
+                        <p><i class="fas fa-envelope mr-3" style=" margin-right : 0.5rem;"> </i>Gmae@gmail.com</p>
+                        <p><i class="fas fa-home mr-3" style=" margin-right : 0.5rem;"></i> 10 Allée Evarsite Galois, 63000</p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-5">
+                        <h6 class="text-uppercase mb-4 font-weight-bold font-size-6rem" style="color: white; font-size : 3rem;">G.M.A.E</h6>
+
+                        <!-- Facebook -->
+                        <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+
+                        <!-- Instagram -->
+                        <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+
+                        <!-- Linkedin -->
+                        <a class="btn btn-primary btn-floating m-1" style="background-color: #0082ca" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <!--Grid row-->
+            </section>
+            <!-- Section: Links -->
         </div>
-    </div>
-    </div>
-</footer>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            <p style=" color : white;">
+                © 2020 Copyright - GMAE </p>
+        </div>
+        <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
+</div>
+<!-- End of .container -->
