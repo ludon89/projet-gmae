57c118f1ca0a3c5c05a227a7817d3f4cda8b71a8
LuDon89 <daudonlucas@gmail.com>
2023-02-27 15:52:44 +0100

    header responsive

--- template/header.php
+++ template/header.php
@@ -1,9 +1,12 @@
-<nav class="navbar navbar-expand-lg  navbar-custom">
-    <div class="container px-5">
-        <img src="../assets/img/logo_GMAE.png" width=100px alt="">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ms-auto">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <div class="logo">
+            <img src="../assets/img/logo_GMAE.png" width=100px alt="">
+            <p>GMAE</p>
+        </div>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link" href="../index.php">Accueil</a></li>
                 <li class="nav-item"><a class="nav-link" href="about.php">Qui sommes-nous?</a></li>
                 <?php if (isset($_SESSION['user'])) : ?>
