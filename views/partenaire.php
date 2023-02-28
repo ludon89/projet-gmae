95ae8b8b12c3c72b26dfcf3ee54f39809509e536
LuDon89 <daudonlucas@gmail.com>
2023-02-28 10:52:56 +0100

    wip addvote

--- views/partenaire.php
+++ views/partenaire.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
+require "../controllers/addvote.php";
+
 // Data
 $idActeur = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
-$idUser = $_SESSION["user"];
-var_dump($idUser);
 
 // Connexion à la BDD
 require('../models/connect-bdd.php');
@@ -53,9 +53,6 @@ FROM
 $req = $bdd->query($sql);
 $dislikes = $req->rowCount();
 
-// Requête et exécution ajout vote (like ou dislike)
-
-
 ?>
 
 <!DOCTYPE html>
@@ -93,20 +90,20 @@ $dislikes = $req->rowCount();
     <section class="mb-5">
         <div class="card bg-light">
             <div class="card-body">
-                <!-- Ajout like & dislike -->
-                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
-                    <input type="radio" class="btn-check" name="1" id="1" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="1">Like</label>
-                    <input type="radio" class="btn-check" name="0" id="0" autocomplete="off">
-                    <label class="btn btn-outline-primary" for="0">Dislike</label>
-                </div>
-                <!-- Ajout commentaire-->
-                <form class="mb-4">
+                <form class="mb-4" method="post" action="../controllers/addvote.php">
+                    <!-- Ajout like & dislike -->
+                    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" class="btn-check" name="1" id="1" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="1">Like</label>
+                        <input type="radio" class="btn-check" name="0" id="0" autocomplete="off">
+                        <label class="btn btn-outline-primary" for="0">Dislike</label>
+                    </div>
+                    <!-- Ajout commentaire-->
                     <textarea class="form-control" rows="3" placeholder="Join the discussion and leave a comment!"></textarea>
                     <button type="submit" class="btn btn-success">Publier</button>
                 </form>
                 <hr>
-                <!-- Single comment-->
+                <!-- Affichage likes et commentaires-->
                 <div class="d-flex">
                     <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
                     <div class="ms-3">
